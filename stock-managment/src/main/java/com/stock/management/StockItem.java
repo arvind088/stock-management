53e68c008a1263ba55e6f7960fbb62c969b50f50
arvind088 <avi.lmc098@gmail.com>
2025-12-18 04:01:21 +0100

    cycle 1 TDD green: implement UUID generation to generate unique ID test

--- stock-managment/src/main/java/com/stock/management/StockItem.java
+++ stock-managment/src/main/java/com/stock/management/StockItem.java
@@ -1,19 +1,23 @@
 package com.stock.management;
 
+import java.util.UUID;
+
 public class StockItem {
 	
+	private final String id;
 	private final String name;
 	private final int quantity;
 	private final double price;
 
 	public StockItem(String name, int quantity, double price) {
+		this.id = UUID.randomUUID().toString();
 		this.name = name;
 		this.quantity = quantity;
 		this.price = price;
 		}
-	
+		
 	public String getId() {
-		return null; 
+		return id; 
 		}
 	
 	public String getName() {
