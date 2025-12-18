1b889ef1a1ee36253fd1fe713ac8d08af586a3e5
arvind088 <avi.lmc098@gmail.com>
2025-12-18 03:21:13 +0100

    Cycle 1 TTD red: fix product initialization to pass tests

--- stock-managment/src/main/java/com/stock/management/StockItem.java
+++ stock-managment/src/main/java/com/stock/management/StockItem.java
@@ -2,19 +2,25 @@ package com.stock.management;
 
 public class StockItem {
 	
-	public StockItem(String name, int quantity, double price) {
-		
-		}
+	private final String name;
+	private final int quantity;
+	private final double price;
 
+	public StockItem(String name, int quantity, double price) {
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
+		}
+	
 	public String getName() {
-		return null;
+		return name;
 		}
 	
 	public int getQuantity() {
-		return 0;
+		return quantity;
 		}
 	
 	public double getPrice() {
-		return 0.0;
+		return price;
 		}
 }
