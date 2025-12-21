41f948e0218173cdae5e3d15b06d0495731ae90e
arvind088 <avi.lmc098@gmail.com>
2025-12-21 05:39:52 +0100

    cycle 3  Red: first failing UI test for txtName presence

---
+++ stock-managment/src/main/java/com/stock/management/StockView.java
@@ -0,0 +1,12 @@
+package com.stock.management;
+
+import javax.swing.JFrame;
+
+public class StockView extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	public StockView() {
+		// title for now to satisfy our first test
+		setTitle("Stock Management App"); 
+		}
+}
