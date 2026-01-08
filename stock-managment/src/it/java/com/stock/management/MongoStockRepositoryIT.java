0ea473f9ba5fbce68952dcc136006e73aa96e1eb
arvind088 <avi.lmc098@gmail.com>
2026-01-08 03:50:25 +0100

    cycle 8 e2e : add e2e t sync integration

--- stock-managment/src/it/java/com/stock/management/MongoStockRepositoryIT.java
+++ stock-managment/src/it/java/com/stock/management/MongoStockRepositoryIT.java
@@ -1,24 +1,21 @@
 package com.stock.management;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import java.util.List;
+
 import org.bson.Document;
-import org.junit.Before;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 
-
+@Testcontainers
 public class MongoStockRepositoryIT {
 
-    // 1. Start the container once for all tests in this file
     @Container
     public static MongoDBContainer mongo = new MongoDBContainer("mongo:6.0.5");
 
@@ -26,8 +23,8 @@ public class MongoStockRepositoryIT {
     private MongoStockRepository repository;
     private MongoCollection<Document> collection;
 
-    @Before
-    public void setup() {
+    @BeforeEach
+    void setup() {
         client = MongoClients.create(mongo.getReplicaSetUrl());
         repository = new MongoStockRepository(client, "test-db", "test-collection");
         collection = client.getDatabase("test-db").getCollection("test-collection");
@@ -35,10 +32,11 @@ public class MongoStockRepositoryIT {
     }
 
     @Test
-    public void testSaveAndFindAll() {
+    void testSaveAndFindAll() {
         StockItem item = new StockItem("Laptop", 5, 1200.0);
+
         repository.save(item);
-        
+
         assertThat(repository.findAll()).containsExactly(item);
     }
 }
